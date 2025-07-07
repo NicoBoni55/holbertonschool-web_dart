d0d329c60f7686a51075bd949e4bb03b2c6ac836
NicoBoni55 <bonifrank55@gmail.com>
2025-07-07 14:24:54 -0300

    fix output

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,6 +1,11 @@
 void outer(String name, String id) {
     String inner() {
-        return "Hello Agent ${name} your id is ${id}";
+        List<String> name_ = name.split(" ");
+        String first_name = name_[1].substring(0, 1);
+        String last_name = name_[0];
+
+        
+        return "Hello Agent ${first_name}.${last_name} your id is ${id}";
     }
     print(inner());
 }
