b70388b389c3ef6f4c0e60933bb3d60c488e4e78
NicoBoni55 <bonifrank55@gmail.com>
2025-07-07 12:29:43 -0300

    sumfunc

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,14 @@
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+String showFunc(int a, int b) {
+    int result1 = add(a, b);
+    int result2 = sub(a, b);
+
+    return "Add ${a} + ${b} = ${result1} \nSub ${a} - ${b} = ${result2}";
+}
