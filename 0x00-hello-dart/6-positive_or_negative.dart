37c8d5d9ea4d5dcd29b5fdd33bda9327223dc24a
NicoBoni55 <bonifrank55@gmail.com>
2025-07-07 11:51:01 -0300

    positive_or_negative

---
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+    int number = int.parse(args[0]);
+
+
+    if (number == 0) {
+        print("${number} is zero");
+    }
+    else if (number > 0) {
+        print("${number} is positive");
+    }
+    else {
+        print("${number} is negative");
+    }
+}
