446b4d1132d7e107a8088d0d516f8159a5fef864
NicoBoni55 <bonifrank55@gmail.com>
2025-07-09 15:44:05 -0300

    basketball shots

---
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -0,0 +1,19 @@
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+    int totalA = teamA['Free throws']! * 1 + 
+                 teamA['2 pointers']! * 2 + 
+                 teamA['3 pointers']! * 3;
+
+    int totalB = teamB['Free throws']! * 1 + 
+                 teamB['2 pointers']! * 2 + 
+                 teamB['3 pointers']! * 3;
+
+    if (totalA > totalB) {
+        return 1;
+    }
+    else if (totalA < totalB) {
+        return 2;
+    }
+    else {
+        return 0;
+    }
+}
