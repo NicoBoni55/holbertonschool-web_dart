3eab2095272f781f755c9a13a5c2cf326359edb6
NicoBoni55 <bonifrank55@gmail.com>
2025-07-10 20:36:30 -0300

    fix return type

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,7 +1,7 @@
 import '4-util.dart';
 import 'dart:convert';
 
-Future<double> calculateTotal() async {
+Future<num> calculateTotal() async {
     try {
         final userDataJson = await fetchUserData();
         final userData = jsonDecode(userDataJson);
