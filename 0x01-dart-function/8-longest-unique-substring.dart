5ca835426dcc19a026e09aff7cd2de4582d179c3
NicoBoni55 <bonifrank55@gmail.com>
2025-07-09 22:09:16 -0300

    longest-unique-substring

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -1,5 +1,6 @@
 String longestUniqueSubstring(String str) {
     String repeat = "";
+    String moreLarge = "";
     for (int i = 0; i < str.length; i++) {
     bool encontrado = false;
         for (int j = 0; j < repeat.length; j++) {
@@ -10,7 +11,16 @@ String longestUniqueSubstring(String str) {
         }
         if(encontrado == false) {
             repeat = repeat + str[i];
+        } else {
+            if (repeat.length > moreLarge.length) {
+                moreLarge = repeat;
+            }
+            int pos = repeat.indexOf(str[i]);
+            repeat = repeat.substring(pos + 1) + str[i];
         }
     }
-    return repeat;
+    if (repeat.length > moreLarge.length) {
+        moreLarge = repeat;
+    }
+    return moreLarge;
 }
